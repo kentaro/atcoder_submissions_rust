0814ea1fdd91f7223b0ac2d6ad8b6586ef371c94
Kentaro Kuribayashi <kentarok@gmail.com>
2023-01-20 22:51:02 +0900

    add abc285 c

--- abc285/src/bin/c.rs
+++ abc285/src/bin/c.rs
@@ -1,3 +1,20 @@
+use num_traits::Pow;
+
 fn main() {
-    todo!();
+  proconio::input! {
+    s: String,
+  }
+
+  solve(s);
+}
+
+fn solve(s: String) {
+  let mut total: u64 = 0;
+
+  for (i, c) in s.chars().rev().enumerate() {
+    let code = c as u64;
+    total += (code - 64) * Pow::pow(26_u64, i);
+  }
+
+  println!("{}", total);
 }
