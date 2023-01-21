67d19d42a014e6259c559d9bca900af871c8909f
Kentaro Kuribayashi <kentarok@gmail.com>
2023-01-22 01:34:20 +0900

    add abc285 b

--- abc285/src/bin/b.rs
+++ abc285/src/bin/b.rs
@@ -1,3 +1,26 @@
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+  proconio::input! {
+    n: usize,
+    s: Chars,
+  }
+
+  solve(n, s);
+}
+
+fn solve(n: usize, s: Vec<char>) {
+  for i in 1..n {
+    let mut l = 0;
+
+    for j in 0..(n - i) {
+      if s[j] != s[j + i] {
+        l += 1;
+      } else {
+        break;
+      }
+    }
+
+    println!("{}", l);
+  }
 }
