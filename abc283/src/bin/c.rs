13ad973a4fed3f47d5afa2c68531e3e485f7bd09
Kentaro Kuribayashi <kentarok@gmail.com>
2023-01-23 20:44:58 +0900

    add abc283 c

--- abc283/src/bin/c.rs
+++ abc283/src/bin/c.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+  input! {
+    s: String,
+  }
+
+  solve(s);
+}
+
+fn solve(s: String) {
+  let mut count = 0;
+  let mut skip = false;
+  let len = s.len();
+
+  for (i, c) in s.chars().enumerate() {
+    if c == '0' && !skip && i < len - 1 {
+      count += 1;
+      skip = true;
+    } else if c == '0' && skip {
+      skip = false;
+    } else {
+      count += 1;
+      skip = false;
+    }
+  }
+
+  println!("{}", count);
 }
