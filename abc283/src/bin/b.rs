2b9210fbfd02544d2465a4e1dc470fab18df742f
Kentaro Kuribayashi <kentarok@gmail.com>
2023-01-23 10:30:41 +0900

    add abc283 b

--- abc283/src/bin/b.rs
+++ abc283/src/bin/b.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+      n: usize,
+      a: [u32; n],
+      q: usize,
+    }
+
+    solve(a, q);
+}
+
+fn solve(mut a: Vec<u32>, q: usize) {
+  for _ in 0..q {
+    input! {
+      l: usize,
+    }
+
+    match l {
+      1 => {
+        input! {
+          query: [u32; 2],
+        }
+
+        a[(query[0] as usize) - 1] = query[1];
+      }
+      2 => {
+        input! {
+          query: [u32; 1],
+        }
+
+        println!("{}", a[(query[0] as usize) - 1]);
+      }
+      _ => {}
+    }
+  }
 }
