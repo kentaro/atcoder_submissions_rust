49576f54fea27fdfe6d5c4203942354b193d59c9
Kentaro Kuribayashi <kentarok@gmail.com>
2023-02-02 15:38:09 +0900

    add abc279 b

--- abc279/src/bin/b.rs
+++ abc279/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+      s: String,
+      t: String,
+    }
+    solve(s, t);
+}
+
+fn solve(s: String, t: String) {
+    let matches: Vec<&str> = s.matches(&t).collect();
+
+    if matches.len() > 0 {
+      println!("Yes")
+    } else {
+      println!("No")
+    }
 }
